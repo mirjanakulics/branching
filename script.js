facd30e532e04608a0d7a724f0df8bc71b677867
Mirjana <kulics.mirjana@gmail.com>
2019-05-08 13:34:18 -0400

    completed form-functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+document.querySelector("form").addEventListener("submit", displayDogName);
+
+function displayDogName() {
+  event.preventDefault();
+  let dogname = document.querySelector("#dogname").value;
+  document.body.append(dogname);
+}
+
